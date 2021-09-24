40839a6e50655e7714543a5104c6b31e599ac6d8
bpime <b.pimed@gmail.com>
2021-09-24 11:46:07 -0500

    exercies

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 const Encrypt = require('./encrypt.js');
 
-// Ejercicio 12
-let data = fs.readFileSync(`./resources/Ejercicio12_text.txt`,'utf-8');
+// Ejercicio 11
+let data = fs.readFileSync(`./resources/Ejercicio11_text.txt`,'utf-8');
 let preprocessed_text_mod27 = Encrypt.preprocessing_mod27(data);
 let encrypt_text_mod27 = Encrypt.vignere(preprocessed_text_mod27, "POSITIVO", 0);
-fs.writeFileSync('./results/Ejercicio12_mod21.txt', encrypt_text_mod27);
+fs.writeFileSync('./results/Ejercicio11_mod21.txt', encrypt_text_mod27);
 
 let preprocessed_text_mod191 = Encrypt.preprocessing_mod191(data);
 let encrypt_text_mod191 = Encrypt.vignere(preprocessed_text_mod191, "POSITIVO", 1);
-fs.writeFileSync('./results/Ejercicio12_mod191.txt', encrypt_text_mod191);
+fs.writeFileSync('./results/Ejercicio11_mod191.txt', encrypt_text_mod191);
 
 
 
