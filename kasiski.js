efda2602bf352c49b44c06c8c357679a60550c9a
jfloreshe <jfloreshe@unsa.edu.pe>
2021-09-24 19:37:31 -0500

    keys

--- kasiski.js
+++ kasiski.js
@@ -1,4 +1,8 @@
 const fs = require("fs")
+const spanishFrequencies = [11.72, 1.49, 3.87, 4.67, 13.72, 0.69, 1.00, 
+							 1.18, 5.28, 0.52, 0.11, 5.24, 3.08, 6.83,
+							 8.44, 2.89, 1.11, 6.41, 7.20, 4.60, 4.55,
+							 1.05, 0.04, 0.14, 1.09, 0.47]
 
 function MCD(values){
 	const lowestValue = Math.min(...values)
@@ -39,31 +43,95 @@ function findRepetitiveStrings(text){
 }
 
 function frequencyTable(text){
+	//Spanish frequencytable
+	const totalSymbols = 26
 	var dictionary = {}
+	var table = []
+	let sumOfCharacters = 0
 	for(let i = 0; i < text.length; i++){
 		let symbol = text[i]
 		if(!(symbol in dictionary))
 			dictionary[symbol] = 1
 		else
 			dictionary[symbol]++
+		sumOfCharacters += 1
 	}
-	console.log(dictionary)
+	for(let i = 0; i < totalSymbols; i++){
+		let charCode = String.fromCharCode(i+ 65)
+		if(charCode in dictionary)
+			table.push(dictionary[charCode])
+		else
+			table.push(0)
+	}
+	//special characters
+	let charCode = 'Ñ'
+	let index = 15
+	if(charCode in dictionary)
+		table.push(dictionary[charCode])
+	else
+		table.push(0)
+	let temp = table[table.length - 1]
+	for(let i = table.length - 1; i >= index; i--){
+		table[i] = table[i-1]		
+	}
+	table[index-1] = temp
+	//converting the frequencies to %
+	for(let i = 0; i< table.length; i++){
+		table[i] /= sumOfCharacters
+		table[i] *= 100
+		table[i] = +(table[i].toFixed(2))
+	}	
+	return table
 }
 
-function getSubcryptogram(text,L){
+function getSubcryptograms(text,L){
+	var subCryptograms = []
 	for(let i = 0; i < L; i++){
-
+		let subCryptogram = ''
+		for(let j = i; j < text.length; j+= L){
+			subCryptogram += text[j]
+		}
+		subCryptograms.push(subCryptogram)
 	}
+	return subCryptograms
 }
-let text = 'LNUDVMUYRMUDVLLPXAFZUEFAIOVWVMUOVMUEVMUEZCUDVSYWCIVCFGUCUNYCGALLGRCYTIJTRNNPJQOPJEMZITYLIAYYKRYEFDUDCAMAVRMZEAMBLEXPJCCQIEHPJTYXVNMLAEZTIMUOFRUFC'
 
-let x = findRepetitiveStrings(text)
-let L = MCD(x)
-frequencyTable(text)
+function getKeyCharacter(languageFrequenciesTable, table){
+	//getting min, we probably need more than 1
+	let minEuclideanDistance = Number.MAX_VALUE
+	let previousIndex = 0
+	let index = 0
+	for(let i = 0; i < languageFrequenciesTable.length - 1; i++){
+		let euclideanDistance = 0
+		for( let j = 0; j < languageFrequenciesTable.length ; j++){
+			let shift = (j + i) % languageFrequenciesTable.length
+			euclideanDistance += Math.pow((languageFrequenciesTable[j]-table[shift]),2)
+		} 
+		euclideanDistance = Math.sqrt(euclideanDistance)
+		if( euclideanDistance < minEuclideanDistance){
+			minEuclideanDistance = euclideanDistance
+			previousIndex = index
+			index = i
+		}			
+	}
+	return [index, previousIndex]
+}
 
-/*
 class Decrypt{
 	static kasiski(text){
+		let repetitiveTable = findRepetitiveStrings(text)
+		let L = MCD(repetitiveTable)
+		let subCryptograms = getSubcryptograms(text, L)
+		console.log(subCryptograms)
+		for(let i =0; i< subCryptograms.length; i++){
+			let table = frequencyTable(subCryptograms[i])	
+			let possibleKeys = getKeyCharacter(spanishFrequencies, table)	
+			console.log(possibleKeys)
+		}
 		
 	}
-}*/
+}
+
+let text = 'LNUDVMUYRMUDVLLPXAFZUEFAIOVWVMUOVMUEVMUEZCUDVSYWCIVCFGUCUNYCGALLGRCYTIJTRNNPJQOPJEMZITYLIAYYKRYEFDUDCAMAVRMZEAMBLEXPJCCQIEHPJTYXVNMLAEZTIMUOFRUFC'
+
+Decrypt.kasiski(text)
